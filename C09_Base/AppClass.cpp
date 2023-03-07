e087bdb0bf9445fc34534043f2e3d4bab8216b02
nrb7617 <nrb7617@MSS3110-06>
2023-03-07 10:47:54 -0500

    Class code

--- C09_Base/AppClass.cpp
+++ C09_Base/AppClass.cpp
@@ -7,6 +7,8 @@ void Application::InitVariables(void)
 	vector3 v3Target = ZERO_V3;
 	vector3 v3Upward = AXIS_Y;
 	m_pCameraMngr->SetPositionTargetAndUpward(v3Position, v3Target, v3Upward);
+	m_sModel = "WallEye.obj";
+	m_pModelMngr->LoadModel(m_sModel);
 }
 void Application::Update(void)
 {
@@ -21,6 +23,18 @@ void Application::Update(void)
 
 	//Update Entity Manager
 	m_pEntityMngr->Update();
+
+	m_pModelMngr->AddModelToRenderList(m_sModel, ToMatrix4(m_qArcBall));
+
+	static float value = 0.0f;
+
+	m_pCameraMngr->SetPosition(vector3(0.0f, 0.0f, 10.0f));	// change position of camera in world (where I am)
+	//m_pCameraMngr->SetTarget(vector3(0.0f, value, 0.0f));		// change rotation of camera (what am i looking at)
+	//m_pCameraMngr->SetUpward(vector3(0.0f, 1.0f, 0.0f));		// change vector that determines what is "up" to the camera (what is upward in my world)
+	//m_pCameraMngr->SetFOV(90.0f);								// change field of view (how much can I see)
+	//m_pCameraMngr->SetNearFarPlanes(0.01f, 15.0f);			// change how much the camera can render in front of it
+
+	value += m_pSystem->GetDeltaTime(0);
 
 	//Add objects to render list
 	m_pEntityMngr->AddEntityToRenderList(-1, true);
